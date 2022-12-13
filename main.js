d720d033faa8d6f7921119becdddb57cee60c43a
MohamedSaid7102 <mohamedshelfwork@gmail.com>
2022-12-13 09:53:31 +0200

    Fixed text color issues & Refactored code

--- main.js
+++ main.js
@@ -2,7 +2,22 @@ const time = document.querySelector('#time');
 const greeting = document.querySelector('#greeting');
 const name = document.querySelector('#name');
 const focus = document.querySelector('#focus');
+const tasks = document.querySelector('#tasks');
+const task = document.querySelector('#task');
 const amPm = document.querySelector('#amPm');
+
+function updateTextColor() {
+  let today = new Date();
+  let Hours = today.getHours();
+  let color = Hours < 12 ? '#111' : '#fff';
+  time.style.color = `${color}`;
+  greeting.style.color = `${color}`;
+  name.style.color = `${color}`;
+  focus.style.color = `${color}`;
+  task.style.color = `${color}`;
+  if (tasks) tasks.style.color = `${color}`;
+  if (amPm) amPm.style.color = `${color}`;
+}
 
 function showTime() {
   // get today date and time
@@ -15,7 +30,9 @@ function showTime() {
   // show the time
   time.innerHTML = `${Hour}<span>:</span>${showZero(
     Min
-  )}<span>:</span>${showZero(Sec)} <span id="amPm">${amPm}</span>`;
+  )}<span>:</span>${showZero(
+    Sec
+  )} <span id="amPm" style="color:inherit">${amPm}</span>`;
   // 12H format
   Hour = Hour % 12 || 12;
   // invoke the function recursevly every one second
@@ -29,21 +46,20 @@ function showZero(number) {
 function showBgImge() {
   let today = new Date();
   let Hours = today.getHours();
-  document.body.style.cssText = `background-size:cover; background-repeat:no-repeat;`;
   if (Hours < 12) {
     // this is morning
-    document.body.style.cssText += `background-image:url(imgs/morning-1.jpg); opacity: 0.6;`;
+    document.body.style.cssText += `background-image:url(imgs/morning-1.jpg);`;
     greeting.textContent = `Good morning`;
+    updateTextColor();
   } else if (Hours < 18) {
     // this is afternoon
     document.body.style.cssText += `background-image:url(imgs/afternoon.jpg);`;
     greeting.textContent = `Good afternoon`;
-    let focus = document.querySelectorAll('h2');
-    focus[0].style.cssText = `color:#fff;`;
-    focus[1].style.cssText = `color:#fff;`;
+    updateTextColor();
   } else {
     // this is evening
-    document.body.style.cssText += `background-image:url(imgs/evening.jpg); color: #fff;`;
+    updateTextColor();
+    document.body.style.cssText += `background-image:url(imgs/evening.jpg);`;
     greeting.textContent = `Good evening`;
   }
 }
@@ -67,6 +83,7 @@ function getName() {
   if (localStorage.getItem('name') === null)
     name.textContent = `[Enter your Name]`;
   else name.textContent = localStorage.getItem('name');
+  updateTextColor();
 }
 
 // set the focus
@@ -89,14 +106,19 @@ function getFocus() {
   if (localStorage.getItem('focus') === null)
     focus.textContent = `[Enter your main task]`;
   else focus.textContent = localStorage.getItem('focus');
+  updateTextColor();
 }
 
-name.addEventListener('keypress', setName);
-name.addEventListener('blur', setName); //if we click outside
-focus.addEventListener('keypress', setFocus);
-focus.addEventListener('blur', setFocus); // if we click outside
+function init() {
+  name.addEventListener('keypress', setName);
+  name.addEventListener('blur', setName); //if we click outside
+  focus.addEventListener('keypress', setFocus);
+  focus.addEventListener('blur', setFocus); // if we click outside
 
-showTime();
-showBgImge();
-getName();
-getFocus();
+  showTime();
+  showBgImge();
+  getName();
+  getFocus();
+}
+
+init();
